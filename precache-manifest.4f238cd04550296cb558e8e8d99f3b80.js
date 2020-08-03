63d35b06e1ed004a89dfac6d22dbe1407c948bfd
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2020-08-03 15:20:23 -0300

    Updates

---
+++ precache-manifest.4f238cd04550296cb558e8e8d99f3b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33dfea5b4c2fdd5250ca09c04f512c92",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "27bd4976826c6d3a203a",
+    "url": "/resume/static/css/main.67ca872c.chunk.css"
+  },
+  {
+    "revision": "a0fbf60f31daec57ee6d",
+    "url": "/resume/static/js/2.c9ecaeb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.c9ecaeb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bd4976826c6d3a203a",
+    "url": "/resume/static/js/main.8f7b1dd2.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  }
+]);
